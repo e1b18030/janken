102b8c1d4b586ec03b2307d043ab28e40a5e22ca
e1b18030 <e1b18030@oit.ac.jp>
2020-11-08 23:58:47 +0900

    試合結果をSELECTで取得して表示する

---
+++ src/main/java/oit/is/z0790/kaizi/janken/model/MatchMapper.java
@@ -0,0 +1,15 @@
+package oit.is.z0790.kaizi.janken.model;
+
+import java.util.ArrayList;
+
+import org.apache.ibatis.annotations.Insert;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Options;
+import org.apache.ibatis.annotations.Select;
+
+@Mapper
+public interface MatchMapper {
+  @Select("select * from matches")
+  ArrayList<Match> selectAllMatches();
+
+}
